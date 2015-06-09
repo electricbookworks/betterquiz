cdc8fd8bc86f0c5b69222fe4e039edddcd6de9a2
craigmj <craig@lateral.co.za>
2015-06-09 13:51:30 +0900

    Spelling correction

--- public/_header.php
+++ public/_header.php
@@ -10,7 +10,7 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
   <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-  <title>BettterCare Quiz</title>
+  <title>BetterCare Quiz</title>
   <link rel="stylesheet" href="css/site.css" >
   <?php CssStore::Link(); ?>
 </head>
